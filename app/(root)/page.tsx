1c849fb1e1488897a94e466b6ba233972a092a14
Yugsingh05 <yugsingh7622@gmail.com>
2024-11-04 16:20:43 +0530

    Hero section complete

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import SearchForm from '../components/SearchForm'
 
-function page() {
+async function Home({searchParams} : {searchParams: Promise<{query?: string}>}) {
+
+    const query = (await searchParams).query
     return (
-        <div>
-            Home   
-        </div>
+        <section className='pink_container'>
+            <h1 className='heading'>
+                Pitch Your Startup,<br/>
+                Connect With EntrePreneurs
+                 </h1>  
+
+                 <p className='sub-heading !max-w-3xl'> Submit Ideas, Vote on Pitches , And get Noticed in Virtual Competitions </p>
+
+                 <SearchForm query = {query}/>
+        </section>
     )
 }
 
-export default page
+export default Home
