5b6643fbee669f29b5968f004a50602392adafb1
Yugsingh05 <yugsingh7622@gmail.com>
2024-11-05 19:54:11 +0530

    startuup landing page complete

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,61 +1,57 @@
-import React from 'react'
-import SearchForm from '../../components/SearchForm'
-import StartupCard from '@/components/StartupCard';
+import React from "react";
+import SearchForm from "../../components/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
-async function Home({searchParams} : {searchParams: Promise<{query?: string}>}) {
+async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const params = {search : query || null}
 
-    const query = (await searchParams).query;
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY,params });
 
-    const posts = [ 
-        {
-            _createdAt : new Date(),
-            _id : 1,
-            views : 55,
-            author : {_id : 1},
-            description : "This is dis",
-            category : "Robots",
-            image : "https://cdn.ebaumsworld.com/mediaFiles/picture/2452130/85386506.jpg",
-            title : "Robot 1",
-        }
-    ]
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup,
+          <br />
+          Connect With EntrePreneurs
+        </h1>
 
-    return (
-        <>
-        <section className='pink_container'>
-            <h1 className='heading'>
-                Pitch Your Startup,<br/>
-                Connect With EntrePreneurs
-                 </h1>  
+        <p className="sub-heading !max-w-3xl">
+          {" "}
+          Submit Ideas, Vote on Pitches , And get Noticed in Virtual
+          Competitions{" "}
+        </p>
 
-                 <p className='sub-heading !max-w-3xl'> Submit Ideas, Vote on Pitches , And get Noticed in Virtual Competitions </p>
+        <SearchForm query={query} />
+      </section>
 
-                 <SearchForm query = {query}/>
-        </section> 
+      {/* // check the add button from shadcn */}
 
-        {/* // check the add button from shadcn */}
+      <section className="section-container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}` : "All startups"}
+        </p>
 
-        <section className='section-container'>
-            <p className='text-30-semibold'>
-                {query ? `Search results for ${query}` : "All startups"}
-            </p>
-
-            <ul className='mt-7 card_grid'>
-                {
-                    posts.length > 0 ?(
-                        posts.map((post :StartupTypeCard) => (
-                            <StartupCard key= {post?._id} post={post}/>
-                        ))
-                    )  : (
-                        <p className='no-results'>No Startups Found</p>
-                    ) 
-                }
-
-
-            </ul>
-
-        </section>
-        </>
-    )
+        <ul className="mt-7 card_grid">
+          {posts.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+      <SanityLive />
+    </>
+  );
 }
 
-export default Home
+export default Home;
