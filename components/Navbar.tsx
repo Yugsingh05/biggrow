cac513a33913ad0207741b7dc50360dea3bd75f1
Yugsingh05 <yugsingh7622@gmail.com>
2024-11-08 23:14:02 +0530

    new style added to logout and create

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,8 +18,14 @@ const Navbar = async () => {
           {session && session?.user ? (
             <>
               <Link href="/startup/create">
-                <span className="max-sm:hidden">Create</span>
-                <BadgePlus className="size-6 sm:hidden" />
+                <BadgePlus className="size-6 sm:hidden !text-white-100" />
+
+                <button className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-white dark:text-white text-white rounded-lg font-bold transform hover:-translate-y-1 transition duration-400 max-sm:hidden ">
+  Create
+</button>
+
+              
+
               </Link>
 
               <form
@@ -30,7 +36,11 @@ const Navbar = async () => {
                 }}
               >
                 <button type="submit">
-                  <span className="max-sm:hidden">Logout</span>
+                  <span className="max-sm:hidden !text-white-100">
+                  <button className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-white dark:text-white text-white rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
+  Create
+</button>
+                  </span>
                   <LogOut className="size-6 sm:hidden text-red-500" />
                 </button>
               </form>
@@ -53,11 +63,11 @@ const Navbar = async () => {
                 await signIn("github");
               }}
             >
-              <button className="p-[3px] relative" type="submit">
-                <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-                <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-                 Login
-                </div>
+              <button className="relative inline-flex h-12 min-w-[120px] overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-5 py-1 text-m font-medium text-white backdrop-blur-3xl">
+                  Login
+                </span>
               </button>
             </form>
           )}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+const addVariablesForColors = ({ addBase, theme }: any) => {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+  
+  // Add the new variables to the base styles
+  addBase({
+    ":root": newVars,
+  });
+};
 
 const config: Config = {
   darkMode: ["class"],
- "animation": {
-            shimmer: "shimmer 2s linear infinite"
-          },
-          "keyframes": {
-            shimmer: {
-              from: {
-                "backgroundPosition": "0 0"
-              },
-              to: {
-                "backgroundPosition": "-200% 0"
-              }
-            }
-          },
+  animation: {
+    shimmer: "shimmer 2s linear infinite",
+  },
+  keyframes: {
+    shimmer: {
+      from: {
+        "backgroundPosition": "200 0",
+      },
+      to: {
+        "backgroundPosition": "-200% 0",
+      },
+    },
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -58,7 +75,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/typography"),
+    addVariablesForColors,
+  ],
 };
 
 export default config;
